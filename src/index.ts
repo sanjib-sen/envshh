8182f7cb6169757301bfbe3fc0b1ef22a1dfb244
Sanjib Kumar Sen <sksenonline@gmail.com>
2023-07-30 08:23:02 +0600

    check

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-
 import { name } from "../package.json";
 process.stdout.write(`Hello from ${name}!\n`);
+import { execSync } from "child_process";
+
+try {
+  const gitVersion = execSync("git --version");
+  process.stdout.write(`${gitVersion}`);
+} catch (error) {
+  process.exit(1);
+}
