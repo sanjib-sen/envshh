258636942a1053a9ee242f9ee03d94a9ee4caab2
Sanjib Kumar Sen <mail@sanjibsen.com>
2023-08-12 14:23:54 +0600

    beforeVisitingTanima

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -3,9 +3,25 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 import { execSync } from "child_process";
+import { log } from "./log.js";
 
-export function runCommand(command: string) {
-  execSync(command, {
-    stdio: "inherit",
-  });
+export function runCommand(command: string, ignoreIfFails: false) {
+  let result;
+  try {
+    result = execSync(command, {
+      stdio: "inherit",
+    });
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      log.error("Got error while running command: " + command);
+      log.error("Command Output: " + (result ? result.toString() : ""));
+      log.error(error.message);
+      if (ignoreIfFails) {
+        return result;
+      } else {
+        process.exit(1);
+      }
+    }
+  }
 }
