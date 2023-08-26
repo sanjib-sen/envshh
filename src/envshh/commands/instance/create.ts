759ba4d247d09cc92e5a6eb8caeb074e048d1d88
Sanjib Kumar Sen <mail@sanjibsen.com>
2023-08-26 23:44:15 +0600

    pre utils

--- src/envshh/commands/instance/create.ts
+++ src/envshh/commands/instance/create.ts
@@ -8,15 +8,14 @@ import * as readlineSync from "readline-sync";
 import { EnvshhInstance } from "../../envshh.js";
 import { exitWithError } from "../../../utils/process.js";
 import { defaultInstanceName } from "../../defaults/defaults.js";
-import { log } from "../../../utils/log.js";
 
 export function createInstance(
-  envshhCreateParams: Partial<EnvshhInstanceType>,
+  envshhCreateParams: Partial<EnvshhInstanceType>
 ) {
   const name =
     envshhCreateParams?.name ||
     readlineSync.question(
-      `Instance Name (Default: ${defaultInstanceName}): `,
+      `Instance Name (Default: ${defaultInstanceName}): `
     ) ||
     defaultInstanceName;
   const mainDirectory =
@@ -24,13 +23,13 @@ export function createInstance(
     readlineSync.question("Directory Path: ");
   if (!mainDirectory) {
     return exitWithError(
-      `Instance ${name} not created. Directory Path is required.`,
+      `Instance ${name} not created. Directory Path is required.`
     );
   }
   const mainRepoUrl =
     envshhCreateParams?.mainRepoUrl ||
     readlineSync.question(
-      "Remote Repository URL (Keep this blank if you want to use offline): ",
+      "Remote Repository URL (Keep this blank if you want to use offline): "
     ) ||
     undefined;
   const envshh = new EnvshhInstance({
@@ -39,6 +38,5 @@ export function createInstance(
     mainRepoUrl: mainRepoUrl,
   });
   const newEnvshh = envshh.create();
-  log.info("Creating..............");
   return newEnvshh.print();
 }
