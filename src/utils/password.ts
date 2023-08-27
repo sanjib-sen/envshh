683b5df6bc87a304c188dd76f1064c8970b418a2
Sanjib Kumar Sen <mail@sanjibsen.com>
2023-08-27 19:11:13 +0600

    preRemove

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -2,6 +2,7 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
 import * as readlineSync from "readline-sync";
 
 export function askPassword(confirm = true) {
