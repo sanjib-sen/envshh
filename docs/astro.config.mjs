9a79ec3efd10f607e9926af7a73bdd1c7710498c
Sanjib Kumar Sen <mail@sanjibsen.com>
2023-09-12 02:31:34 +0600

    set base path doc

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sanjib-sen.github.io/envshh',
+	base: '/envshh',
 	integrations: [
 		starlight({
 			favicon: '/favicon.ico',
